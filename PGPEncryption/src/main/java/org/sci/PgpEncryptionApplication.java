970fc0bf56050f47253a3ac5a9ae25abd58ce079
sisirakumar <sisirakumardash8@gmail.com>
2020-11-03 06:33:26 +0530

    first commit on PGP encryption

---
+++ PGPEncryption/src/main/java/org/sci/PgpEncryptionApplication.java
@@ -0,0 +1,13 @@
+package org.sci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgpEncryptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgpEncryptionApplication.class, args);
+	}
+
+}
